d9a41acdb5eb82dac7da099589e7a9b21fb45ddb
Xennis <code@xennis.org>
2020-05-27 23:23:29 +0200

    Add G28 rewrite

--- octoprint_restore_leveling_after_g28/__init__.py
+++ octoprint_restore_leveling_after_g28/__init__.py
@@ -1,37 +1,19 @@
 # coding=utf-8
 from __future__ import absolute_import
 
-### (Don't forget to remove me)
-# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
-# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
-# defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
-# as necessary.
-#
-# Take a look at the documentation on what other plugin mixins are available.
+import logging
 
 import octoprint.plugin
 
-class Restore_leveling_after_g28Plugin(octoprint.plugin.SettingsPlugin,
-                                       octoprint.plugin.AssetPlugin,
-                                       octoprint.plugin.TemplatePlugin):
+class RestoreLevelingAfterG28Plugin():
 
-	##~~ SettingsPlugin mixin
+	def rewrite_g28(self, comm_instance, phase, cmd, cmd_type, gcode, *args, **kwargs):
+		if not gcode or gcode != "G28":
+			return
 
-	def get_settings_defaults(self):
-		return dict(
-			# put your plugin's default settings here
-		)
-
-	##~~ AssetPlugin mixin
-
-	def get_assets(self):
-		# Define your plugin's asset files to automatically include in the
-		# core UI here.
-		return dict(
-			js=["js/restore_leveling_after_g28.js"],
-			css=["css/restore_leveling_after_g28.css"],
-			less=["less/restore_leveling_after_g28.less"]
-		)
+		cmd = [(cmd,), ("M420 S1",)]
+		self._logger.info("Expand G28: {cmd}".format(**locals()))
+		return cmd
 
 	##~~ Softwareupdate hook
 
@@ -41,39 +23,29 @@ class Restore_leveling_after_g28Plugin(octoprint.plugin.SettingsPlugin,
 		# for details.
 		return dict(
 			restore_leveling_after_g28=dict(
-				displayName="Restore_leveling_after_g28 Plugin",
+				displayName="Restore Leveling After G28",
 				displayVersion=self._plugin_version,
 
 				# version check: github repository
 				type="github_release",
 				user="Xennis",
-				repo="Restore Leveling After G28",
+				repo="OctoPrint-RestoreLevelingAfterG28",
 				current=self._plugin_version,
 
 				# update method: pip
-				pip="https://github.com/Xennis/Restore Leveling After G28/archive/{target_version}.zip"
+				pip="https://github.com/Xennis/OctoPrint-RestoreLevelingAfterG28/archive/{target_version}.zip"
 			)
 		)
 
-
-# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
-# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
-# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
-__plugin_name__ = "Restore_leveling_after_g28 Plugin"
-
-# Starting with OctoPrint 1.4.0 OctoPrint will also support to run under Python 3 in addition to the deprecated
-# Python 2. New plugins should make sure to run under both versions for now. Uncomment one of the following
-# compatibility flags according to what Python versions your plugin supports!
-#__plugin_pythoncompat__ = ">=2.7,<3" # only python 2
-#__plugin_pythoncompat__ = ">=3,<4" # only python 3
-#__plugin_pythoncompat__ = ">=2.7,<4" # python 2 and 3
+__plugin_name__ = "Restore Leveling After G28"
+__plugin_pythoncompat__ = ">=2.7,<4"
 
 def __plugin_load__():
 	global __plugin_implementation__
-	__plugin_implementation__ = Restore_leveling_after_g28Plugin()
+	__plugin_implementation__ = RestoreLevelingAfterG28Plugin()
 
 	global __plugin_hooks__
 	__plugin_hooks__ = {
+		"octoprint.comm.protocol.gcode.queuing": __plugin_implementation__.rewrite_g28,
 		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
 	}
-
