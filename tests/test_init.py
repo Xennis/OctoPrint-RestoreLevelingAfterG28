c09e1b9bf283c0347f92513f8bcd26b3b833d6d6
Xennis <code@xennis.org>
2020-06-01 23:58:09 +0200

    Add tests for hook_atcommand_sending

--- tests/test_init.py
+++ tests/test_init.py
@@ -5,6 +5,53 @@ from mock import MagicMock
 from octoprint_restore_leveling_after_g28 import RestoreLevelingAfterG28Plugin
 
 
+class TestHookAtcommandSending(unittest.TestCase):
+
+	def test_cmd_none(self):
+		cmd = None
+		p = RestoreLevelingAfterG28Plugin()
+		p._logger = MagicMock()
+		actual = p.hook_atcommand_sending(None, None, cmd, None)
+		self.assertIsNone(actual)
+
+	def test_some_cmd(self):
+		cmd = "some_cmd"
+		p = RestoreLevelingAfterG28Plugin()
+		p._logger = MagicMock()
+		actual = p.hook_atcommand_sending(None, None, cmd, None)
+		self.assertIsNone(actual)
+
+	def test_cmd_leveling_off(self):
+		cmd = "restore_leveling"
+		p = RestoreLevelingAfterG28Plugin()
+		p._logger = MagicMock()
+		actual = p.hook_atcommand_sending(None, None, cmd, None)
+		self.assertIsNone(actual)
+		p._logger.debug.assert_called_once_with("Keep leveling disabled")
+
+	def test_cmd_leveling_on(self):
+		cmd = "restore_leveling"
+		p = RestoreLevelingAfterG28Plugin()
+		p.leveling_enabled = True
+		p._logger = MagicMock()
+		p._printer = MagicMock()
+		actual = p.hook_atcommand_sending(None, None, cmd, None, tags=["tag1", "tag2"])
+		self.assertIsNone(actual)
+		p._logger.info.assert_called_once_with("Re-enable leveling: M420 S1")
+		p._printer.commands.assert_called_once_with("M420 S1", tags=["tag1", "tag2"])
+
+	def test_cmd_leveling_on_tags_none(self):
+		cmd = "restore_leveling"
+		p = RestoreLevelingAfterG28Plugin()
+		p.leveling_enabled = True
+		p._logger = MagicMock()
+		p._printer = MagicMock()
+		actual = p.hook_atcommand_sending(None, None, cmd, None, tags=None)
+		self.assertIsNone(actual)
+		p._logger.info.assert_called_once_with("Re-enable leveling: M420 S1")
+		p._printer.commands.assert_called_once_with("M420 S1", tags=None)
+
+
 class TestHookGcodeReceived(unittest.TestCase):
 
 	def test_none(self):
@@ -25,7 +72,6 @@ class TestHookGcodeReceived(unittest.TestCase):
 		line = "echo:Bed Leveling On"
 		p = RestoreLevelingAfterG28Plugin()
 		p._logger = MagicMock()
-		p._logger.info = MagicMock()
 		actual = p.hook_gcode_received(None, line, None)
 		self.assertEqual(line, actual, msg="line")
 		self.assertTrue(p.leveling_enabled, msg="leveling enabled")
@@ -35,7 +81,6 @@ class TestHookGcodeReceived(unittest.TestCase):
 		line = "echo:Bed Leveling Off"
 		p = RestoreLevelingAfterG28Plugin()
 		p._logger = MagicMock()
-		p._logger.info = MagicMock()
 		actual = p.hook_gcode_received(None, line, None)
 		self.assertEqual(line, actual, msg="line")
 		self.assertFalse(p.leveling_enabled, msg="leveling enabled")
